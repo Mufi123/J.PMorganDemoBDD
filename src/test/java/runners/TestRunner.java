33a75a3ae29c7e826ea525978e04801b6902ac13
mufi <mustafaachu@gmail.com>
2023-11-14 14:44:00 +0200

    monochrome false for differentiate result

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features/GoogleSearch.feature",
 glue= {"steps", "hooks"},
-monochrome= true,
+monochrome= false,
 plugin = {"pretty", "junit:target/JUnitReports/report.xml",
 		"json:target/JSONReports/report.json",
 		"html:target/HtmlReports", "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"}
